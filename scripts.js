4067b3573efc132f9ca021150d3dd16477e5fd5d
heyericamay <erica.may25@hotmail.com>
2025-03-28 11:03:11 -0400

    default tab fix

--- scripts.js
+++ scripts.js
@@ -17,7 +17,8 @@ function openSession(evt, sessionName) {
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(sessionName).style.display = "block";
     evt.currentTarget.className += " active";
+
+    // Get the element with id="defaultOpen" and click on it
+    document.getElementById("defaultOpen").click();
   } 
 
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
